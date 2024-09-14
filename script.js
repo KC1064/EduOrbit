f84d80ddd63222cf71b2e5657e4bd3f3f79ab171
KC1064 <kiron08072003@gmail.com>
2024-09-14 14:30:06 +0530

    Completed the landing page

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-gsap.to("circle", {
-  rotation: "360",
-  ease: Linear.easeNone,
-  repeat: -1,
-  duration:6,
+var tl = gsap.timeline();
+
+tl.from("#logo", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
 });
+
+tl.from("#nav-links", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.2,
+});
+
+tl.from("#reg-btn", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.2,
+});
+
+tl.from("#hero-section", {
+  y: 60,
+  opacity: 0,
+  duration: 0.5,
+});
+
+tl.from("#stat-nums", {
+  y: 30,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.2,
+});
+
+tl.to("#hero-text span", {
+  color: "#24e0b4",
+  fontStyle: "italic",
+  textShadow: "0px 0px 10px #24e0b4",
+  duration: 2,
+  stagger: 0.2,
+  ease: "power2.out",
+});
+
+tl.to("#hero-text span",{
+  keyframes: [
+    { textShadow: "0px 0px 5px #24e0b4", duration: 0.1 },
+    { textShadow: "0px 0px 15px #24e0b4", duration: 0.2 },
+    { textShadow: "0px 0px 10px #24e0b4", duration: 0.1 },
+    { textShadow: "0px 0px 20px #24e0b4", duration: 0.2 },
+    { textShadow: "0px 0px 10px #24e0b4", duration: 0.2 }
+  ],
+  repeat:-1,
+  duration: 2
+})
