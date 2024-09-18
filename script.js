5118670b838778f00b686e5290d535b61659dd0d
KC1064 <kiron08072003@gmail.com>
2024-09-18 09:49:01 +0530

    Fine tunining

--- script.js
+++ script.js
@@ -37,54 +37,60 @@ tl.to("#hero-text span", {
   color: "#24e0b4",
   fontStyle: "italic",
   textShadow: "0px 0px 10px #24e0b4",
-  duration: 2,
+  duration: 1,
   stagger: 0.2,
-  ease: "power2.out",
 });
 
-tl.to("#hero-text span",{
+tl.to("#hero-text span", {
   keyframes: [
-    { textShadow: "0px 0px 5px #24e0b4", duration: 0.1 },
-    { textShadow: "0px 0px 15px #24e0b4", duration: 0.2 },
-    { textShadow: "0px 0px 10px #24e0b4", duration: 0.1 },
+    { textShadow: "0px 0px 15px #24e0b4", duration: 0.1 },
     { textShadow: "0px 0px 20px #24e0b4", duration: 0.2 },
-    { textShadow: "0px 0px 10px #24e0b4", duration: 0.2 }
+    { textShadow: "0px 0px 30px #24e0b4", duration: 0.1 },
+    { textShadow: "0px 0px 20px #24e0b4", duration: 0.2 },
+    { textShadow: "0px 0px 15px #24e0b4", duration: 0.2 },
   ],
-  repeat:-1,
-  duration: 2
-})
-
-
-document.getElementById('reg-btn').addEventListener('click', function() {
-  window.location.href = 'pages/register.html';
+  repeat: -1,
+  duration: 1,
+  ease: "power1.inOut",
 });
 
-document.getElementById('logo').addEventListener('click', function() {
-  window.location.href = 'index.html';
+document.getElementById("reg-btn").addEventListener("click", function () {
+  window.location.href = "pages/register.html";
+});
+
+document.getElementById("logo").addEventListener("click", function () {
+  window.location.href = "index.html";
 });
 
 function showMenu() {
-  const navMob = document.querySelector('.nav-mob');
-  if (navMob.style.display === 'flex') {
-      navMob.style.display = 'none';
+  const navMob = document.querySelector(".nav-mob");
+  if (navMob.style.display === "flex") {
+    navMob.style.display = "none";
   } else {
-      navMob.style.display = 'flex';
+    navMob.style.display = "flex";
   }
 }
 
 function hidemenu() {
-  const navMob = document.querySelector('.nav-mob');
-  if (navMob.style.display === 'flex') {
-      navMob.style.display = 'none';
+  const navMob = document.querySelector(".nav-mob");
+  if (navMob.style.display === "flex") {
+    navMob.style.display = "none";
   } else {
-      navMob.style.display = 'flex';
+    navMob.style.display = "flex";
   }
 }
 
+var main = document.querySelector("main");
+var cursor = document.querySelector("#cursor");
 
+main.addEventListener("mousemove", function (e) {
+  console.log("Clicked");
+  gsap.to(cursor, {
+    x: e.clientX,
+    y: e.clientY,
+    duration: 1,
+    scale: 1,
+  });
+});
 
-
-// gasp.from("#cross",{
-//   opacity: 0,
-//   x: 30,
-// })
+  
